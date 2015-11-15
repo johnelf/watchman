5ce55bb4723502968e1be4730813d8d3d303912a
Zhang Jinwen <jwzhang@thoughtworks.com>
2015-11-15 22:34:54 +0800

    invalidate keywords, add usages

--- src/main/java/com/jwzhang/AppMain.java
+++ src/main/java/com/jwzhang/AppMain.java
@@ -7,20 +7,23 @@ import com.jwzhang.io.GitHubRegex;
 import com.jwzhang.model.github.GitHubItem;
 import com.jwzhang.model.github.GitHubSearchResult;
 import com.jwzhang.model.user.User;
+import com.sun.javaws.exceptions.InvalidArgumentException;
 import org.apache.http.impl.client.HttpClientBuilder;
 
 import java.io.*;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 public class AppMain {
-    private static String keywords = "realestate.com.au";
+    private static String keywords = "keywords";
 
     public static void main(String[] args) throws IOException {
 
-        if (args.length < 1) {
+        if (args.length < 2) {
             System.out.println("Two Arguments are required.");
+            System.out.println("Usage: java -jar <watchman.jar> <input csv file> <key words>");
             return;
         }
 
@@ -31,13 +34,14 @@ public class AppMain {
 
         List<User> users = csvInputParser.parse(
             Optional.of(args[0]).map(AppMain::validateFileName).map(AppMain::getFileName).get());
+        keywords = Optional.of(args[1]).map(AppMain::processKeyWords).get();
 
         String targetFile = Paths.get("").toAbsolutePath().toString() + "/report.html";
 
         BufferedWriter output = null;
         File file = new File(targetFile);
         output = new BufferedWriter(new FileWriter(file));
-        output.write("<head title=" + keywords + "\"><head>\n<body>");
+        output.write("<head title=" + AppMain.keywords + "\"><head>\n<body>");
         String userNames = "";
         users.removeIf(u -> u.getAccount().equals("") || u.getName().equals(""));
         for (User user: users) {
@@ -45,12 +49,12 @@ public class AppMain {
             userNames += "user:" + userName + " ";
         }
 
-        GitHubSearchResult searchResult = sensitiveWordsWatchman.watch(userNames + keywords);
+        GitHubSearchResult searchResult = sensitiveWordsWatchman.watch(userNames + AppMain.keywords);
         try {
             for (GitHubItem item : searchResult.getItems()) {
                 output.write("<a href=");
                 output.write(item.getHtmlUrl());
-                output.write(" >" + item.getGitUrl() + "</a><br>");
+                output.write(" >" + item.getRepo().getOwner().getLogin() + "</a><br>");
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -58,6 +62,15 @@ public class AppMain {
 
         output.write("</body>");
         output.close();
+    }
+
+    private static String processKeyWords(String keywords){
+        if (keywords == null || keywords.equals("")) {
+            System.out.println("Keywords should not be empty or null");
+            System.out.println("Use token as default value");
+            return "token";
+        }
+        return keywords;
     }
 
     private static String validateFileName(String n) {
